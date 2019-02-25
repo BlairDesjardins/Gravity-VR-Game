963ad009f0eeeb725c783cb46f6ecef77c0d7a70
Blair Desjardins <blairmichaeldesjardins@gmail.com>
2019-02-24 23:33:35 -0500

    create repo for gravity VR game

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class Laser : MonoBehaviour
+    {
+
+        public SteamVR_Action_Boolean laserAction;
+
+        public Hand hand;
+
+        public LineRenderer laser;
+
+        private void OnEnable()
+        {
+            if (hand == null)
+                hand = this.GetComponent<Hand>();
+
+            if (laserAction == null)
+            {
+                Debug.LogError("No laser action assigned");
+                return;
+            }
+
+            laserAction.AddOnChangeListener(OnLaserActionChange, hand.handType);
+
+            if (laser == null)
+                laser = this.GetComponent<LineRenderer>();
+        }
+
+        private void OnDisable()
+        {
+            if (laserAction != null)
+                laserAction.RemoveOnChangeListener(OnLaserActionChange, hand.handType);
+        }
+
+        private void OnLaserActionChange(SteamVR_Action_In actionIn)
+        {
+            if (laserAction.GetStateDown(hand.handType))
+            {
+                laser.enabled = true;
+            }
+            else if (laserAction.GetStateUp(hand.handType))
+            {
+                laser.enabled = false;
+            }
+        }
+
+        private void Update()
+        {
+            if (laser.enabled)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+                {
+                    laser.SetPosition(0, transform.position);
+                    laser.SetPosition(1, hit.point);
+                }
+                else
+                {
+                    laser.SetPosition(0, transform.position);
+                    laser.SetPosition(1, transform.TransformDirection(Vector3.forward) * 1000);
+                }
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class Spawner : MonoBehaviour
+    {
+
+        public SteamVR_Action_Boolean spawnSphere;
+        public GameObject gravitySphere;
+        public GameObject transparentGravitySphere;
+
+        private Hand hand;
+        
+        private Vector3 radiusStartPoint;
+        private Vector3 radiusEndPoint;
+        private float scaleFactor = 20f;
+
+        private GameObject transparentEarth;
+
+        private void OnEnable()
+        {
+            if (hand == null)
+                hand = this.GetComponent<Hand>();
+
+            if (spawnSphere == null)
+            {
+                Debug.LogError("No spawn sphere action assigned");
+                return;
+            }
+
+            spawnSphere.AddOnChangeListener(OnSpawnSphereChange, hand.handType);
+            spawnSphere.AddOnUpdateListener(OnSpawnSphereUpdate, hand.handType);
+        }
+
+        private void OnDisable()
+        {
+            if (spawnSphere != null)
+            {
+                spawnSphere.RemoveOnChangeListener(OnSpawnSphereChange, hand.handType);
+                spawnSphere.RemoveOnUpdateListener(OnSpawnSphereUpdate, hand.handType);
+            }
+        }
+
+        private void OnSpawnSphereChange(SteamVR_Action_In actionIn)
+        {
+            if (spawnSphere.GetStateDown(hand.handType))
+            {
+                radiusStartPoint = transform.position;
+                transparentEarth = Instantiate(transparentGravitySphere, radiusStartPoint, new Quaternion());
+            }
+            else if (spawnSphere.GetStateUp(hand.handType))
+            {
+                radiusEndPoint = transform.position;
+                GameObject newGravityObject = Instantiate(gravitySphere, radiusStartPoint, new Quaternion());
+                float gravityObjectRadius = Vector3.Distance(radiusStartPoint, radiusEndPoint);
+                float gravityObjectScale = gravityObjectRadius / scaleFactor;
+                newGravityObject.transform.localScale = new Vector3(gravityObjectScale, gravityObjectScale, gravityObjectScale);
+                newGravityObject.GetComponent<Rigidbody>().mass = gravityObjectRadius * scaleFactor;
+                if (transparentEarth)
+                {
+                    Destroy(transparentEarth);
+                }
+            }
+        }
+
+        private void OnSpawnSphereUpdate(SteamVR_Action_In actionIn)
+        {
+            if (transparentEarth)
+            {
+                float gravityObjectRadius = Vector3.Distance(transparentEarth.transform.position, transform.position);
+                float gravityObjectScale = gravityObjectRadius / scaleFactor;
+                transparentEarth.transform.localScale = new Vector3(gravityObjectScale, gravityObjectScale, gravityObjectScale);
+            }
+        }
+    }
+}
