643edece3930c52f7efb61e60e6656a2c65aa1cb
Blair Desjardins <blairmichaeldesjardins@gmail.com>
2019-03-12 15:17:07 -0400

    add blackhole, other features

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class Planet : GravityObject
+    {
+
+        private float gravityModifier = 0.06674f; //0.00000000006674f;
+        private Rigidbody cubeRB;
+
+        private void Start()
+        {
+            cubeRB = GetComponent<Rigidbody>();
+        }
+
+        private void OnAttachedToHand(Hand hand)
+        {
+            gravityActive = true;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            gravityActive = true;
+        }
+
+        private void Update()
+        {
+            if (gravityActive)
+            {
+                GameObject[] gravityObjects = GameObject.FindGameObjectsWithTag("GravityObject");
+
+                foreach (GameObject gravityObject in gravityObjects)
+                {
+                    if (gravityObject.GetComponent<GravityObject>().IsGravityActive())
+                    {
+                        float distance = Vector3.Distance(gravityObject.transform.position, transform.position);
+
+                        if (distance >= 0.001f)
+                        {
+                            Vector3 gravityVector = (gravityObject.transform.position - transform.position).normalized;
+
+                            float gravityMagnitude = (gravityModifier * cubeRB.mass * gravityObject.GetComponent<Rigidbody>().mass) / (distance * distance);
+
+                            Vector3 gravityForce = new Vector3(gravityVector.x * gravityMagnitude, gravityVector.y * gravityMagnitude, gravityVector.z * gravityMagnitude);
+
+                            cubeRB.AddForce(gravityForce);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
