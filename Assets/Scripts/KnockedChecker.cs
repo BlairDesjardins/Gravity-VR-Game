76870016728183be1069d57725add058fbe5e1e1
Blair Desjardins <blairmichaeldesjardins@gmail.com>
2019-03-14 17:04:02 -0400

    add check for knocked down pins

---
+++ Assets/Scripts/KnockedChecker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockedChecker : MonoBehaviour {
+
+    private float rayDistance = 0.4f;
+    private bool knockedDown;
+
+	void Update () {
+        RaycastHit hit;
+        LayerMask mask = LayerMask.GetMask("Pin Detector");
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit, rayDistance, mask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * hit.distance, Color.yellow);
+            knockedDown = false;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * rayDistance, Color.red);
+            knockedDown = true;
+        }
+	}
+
+    public bool getKnockedDown()
+    {
+        return knockedDown;
+    }
+}
---
+++ Assets/Scripts/WinChecker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinChecker : MonoBehaviour {
+
+    private GameObject[] pins;
+    private float timeForWin = 3f;
+    private float winTimer;
+
+	void Start () {
+        pins = GameObject.FindGameObjectsWithTag("Pin");
+        winTimer = timeForWin;
+	}
+	
+	void Update () {
+        int pinsKnocked = 0;
+		foreach (GameObject pin in pins)
+        {
+            if (pin.GetComponent<KnockedChecker>().getKnockedDown())
+                pinsKnocked++;
+            else
+                winTimer = timeForWin;
+            if (pinsKnocked == pins.Length)
+                winTimer -= Time.deltaTime;
+        }
+
+        if (winTimer <= 0)
+            Victory();
+	}
+
+    private void Victory()
+    {
+        Debug.Log("Vi Won!");
+    }
+}
